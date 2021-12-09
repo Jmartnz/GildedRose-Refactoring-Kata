057e9f40977188708887f097ffb842401afed730
Josue Martinez Fernandez <jmartnz.designer@gmail.com>
2021-12-09 15:55:39 +0100

    Make TexttestFixture a test

--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,8 +1,19 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TexttestFixture {
-    public static void main(String[] args) {
-        System.out.println("OMGHAI!");
+
+    @Test
+    public void test() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(outputStream);
+        out.println("OMGHAI!");
 
         Item[] items = new Item[] {
                 new Item("+5 Dexterity Vest", 10, 20), //
@@ -19,19 +30,44 @@ public class TexttestFixture {
         GildedRose app = new GildedRose(items);
 
         int days = 2;
-        if (args.length > 0) {
-            days = Integer.parseInt(args[0]) + 1;
-        }
 
         for (int i = 0; i < days; i++) {
-            System.out.println("-------- day " + i + " --------");
-            System.out.println("name, sellIn, quality");
+            out.println("-------- day " + i + " --------");
+            out.println("name, sellIn, quality");
             for (Item item : items) {
-                System.out.println(item);
+                out.println(item);
             }
-            System.out.println();
+            out.println();
             app.updateQuality();
         }
+
+        assertEquals(expected, outputStream.toString());
     }
 
+  private final String expected =
+      "OMGHAI!" + System.lineSeparator()
+          + "-------- day 0 --------" + System.lineSeparator()
+          + "name, sellIn, quality" + System.lineSeparator()
+          + "+5 Dexterity Vest, 10, 20" + System.lineSeparator()
+          + "Aged Brie, 2, 0" + System.lineSeparator()
+          + "Elixir of the Mongoose, 5, 7" + System.lineSeparator()
+          + "Sulfuras, Hand of Ragnaros, 0, 80" + System.lineSeparator()
+          + "Sulfuras, Hand of Ragnaros, -1, 80" + System.lineSeparator()
+          + "Backstage passes to a TAFKAL80ETC concert, 15, 20" + System.lineSeparator()
+          + "Backstage passes to a TAFKAL80ETC concert, 10, 49" + System.lineSeparator()
+          + "Backstage passes to a TAFKAL80ETC concert, 5, 49" + System.lineSeparator()
+          + "Conjured Mana Cake, 3, 6" + System.lineSeparator()
+          + System.lineSeparator()
+          + "-------- day 1 --------" + System.lineSeparator()
+          + "name, sellIn, quality" + System.lineSeparator()
+          + "+5 Dexterity Vest, 9, 19" + System.lineSeparator()
+          + "Aged Brie, 1, 1" + System.lineSeparator()
+          + "Elixir of the Mongoose, 4, 6" + System.lineSeparator()
+          + "Sulfuras, Hand of Ragnaros, 0, 80" + System.lineSeparator()
+          + "Sulfuras, Hand of Ragnaros, -1, 80" + System.lineSeparator()
+          + "Backstage passes to a TAFKAL80ETC concert, 14, 21" + System.lineSeparator()
+          + "Backstage passes to a TAFKAL80ETC concert, 9, 50" + System.lineSeparator()
+          + "Backstage passes to a TAFKAL80ETC concert, 4, 50" + System.lineSeparator()
+          + "Conjured Mana Cake, 2, 5" + System.lineSeparator()
+          + System.lineSeparator();
 }

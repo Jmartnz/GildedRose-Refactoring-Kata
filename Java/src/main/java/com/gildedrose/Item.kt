46a92d1889617bcf7c0939e56d45d039b3fc0c78
Josue Martinez Fernandez <jmartnz.designer@gmail.com>
2021-12-10 18:06:33 +0100

    Introduced subclasses and update1 method

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -1,9 +1,120 @@
 package com.gildedrose
 
-class Item(
+open class Item(
     val name: String,
-    var sellIn: Int = 0,
-    var quality: Int = 0
+    var sellIn: Int,
+    var quality: Int
 ) {
     override fun toString(): String = "$name, $sellIn, $quality"
+}
+
+open class BaseItem(
+    name: String,
+    sellIn: Int = 0,
+    quality: Int = 0
+) : Item(name, sellIn, quality) {
+
+    fun update() {
+        update1()
+        update2()
+        update3()
+    }
+
+    private fun update3() {
+        if (sellIn < 0) {
+            if (name != "Aged Brie") {
+                if (name != "Backstage passes to a TAFKAL80ETC concert") {
+                    if (quality > 0) {
+                        if (name != "Sulfuras, Hand of Ragnaros") {
+                            this.quality = quality - 1
+                        }
+                    }
+                } else {
+                    this.quality = quality - quality
+                }
+            } else {
+                if (quality < 50) {
+                    this.quality = quality + 1
+                }
+            }
+        }
+    }
+
+    private fun update2() {
+        if (name != "Sulfuras, Hand of Ragnaros") {
+            this.sellIn = sellIn - 1
+        }
+    }
+
+    protected open fun update1() {
+        if (name != "Aged Brie"
+                && name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (quality > 0) {
+                if (name != "Sulfuras, Hand of Ragnaros") {
+                    this.quality = quality - 1
+                }
+            }
+        } else {
+            if (quality < 50) {
+                this.quality = quality + 1
+                if (name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (sellIn < 11) {
+                        if (quality < 50) {
+                            this.quality = quality + 1
+                        }
+                    }
+                    if (sellIn < 6) {
+                        if (quality < 50) {
+                            this.quality = quality + 1
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+class Brie(
+    name: String,
+    sellIn: Int,
+    quality: Int
+) : BaseItem(name, sellIn, quality) {
+
+    override fun update1() {
+        if (quality < 50) {
+            this.quality = quality + 1
+        }
+    }
+}
+
+class Pass(
+    name: String,
+    sellIn: Int,
+    quality: Int
+) : BaseItem(name, sellIn, quality) {
+
+    override fun update1() {
+        if (quality < 50) {
+            this.quality = quality + 1
+            if (sellIn < 11) {
+                if (quality < 50) {
+                    this.quality = quality + 1
+                }
+            }
+            if (sellIn < 6) {
+                if (quality < 50) {
+                    this.quality = quality + 1
+                }
+            }
+        }
+    }
+}
+
+class Sulfuras(
+    name: String,
+    sellIn: Int,
+    quality: Int
+) : BaseItem(name, sellIn, quality) {
+
+    override fun update1() {}
 }
--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -15,17 +15,17 @@ public class TexttestFixture {
         PrintStream out = new PrintStream(outputStream);
         out.println("OMGHAI!");
 
-        Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        Item[] items = new BaseItem[] {
+                new BaseItem("+5 Dexterity Vest", 10, 20), //
+                new Brie("Aged Brie", 2, 0), //
+                new BaseItem("Elixir of the Mongoose", 5, 7), //
+                new Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Sulfuras("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Pass("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Pass("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Pass("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new BaseItem("Conjured Mana Cake", 3, 6) };
 
         GildedRose app = new GildedRose(items);
 
